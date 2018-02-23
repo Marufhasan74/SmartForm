ea71e55ec37a96460a322fd30876d35df1aad031
Marufhasan74 <marufiub9@gmail.com>
2018-02-24 02:04:37 +0600

    Assignment 4

--- smartForm.js
+++ smartForm.js
@@ -1,8 +1,50 @@
-// Your jQuery goes here
+var userData = {
+  name : '' ,
+  email: '',
+  html:{likes:[], dislikes:[]},
+  js:{likes:[], dislikes:[]},
+  css:{likes:[], dislikes:[]},
+  strength:{css:'',js:'',html:''},
+  currentQuestion:'#welcome'
+};
+console.log(localStorage.getItem('userData'));
+if(localStorage.getItem(userData)){
+  userData=JSON .parse(localStorage.getItem('userData'));
+  $(userData.currentQuestion).show();
+$('#welcome').hide();
+$('#name').val(userData.name);
+$('#email').val(userData.email);
+}
+else{
+  localStorage.setItem('userData',JSON.stringify(userData));
+}
 
-var userData = {};
 $('#start').click(function(){
-	console.log("start");
-	$('#welcome').hide();
-	$('#q1').show();
+ 
+  $('#welcome').hide();
+  $('#q1').show();
+});
+$('#name').change(function (event) {
+  console.log($('#name').val());
+})
+
+$('#email').change(function (event) {
+  console.log($('#email').val());
+})
+
+$('#q1Next').click(function(){
+  
+  if($('#name').val() &&  $('#email').val()){
+  userData.name = $('#name').val();
+  userData.email = $('#email').val();
+  localStorage.setItem('userData',JSON.stringify(userData));
+  $('#q1').hide();
+  $('#q2').show();
+}
+else{
+  alert('please enter your name and email');
+  $('#q1').hide();
+  $('#q2').show();
+
+}
 });
