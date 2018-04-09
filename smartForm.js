4b0152de09b2fb5bf5f4fb6a6bec5aec41c7a760
Marufhasan74 <marufiub9@gmail.com>
2018-04-10 01:03:37 +0600

    Assignment 7 & 8

--- smartForm.js
+++ smartForm.js
@@ -1,50 +1,360 @@
+/*Maruf Hasan,1531005*/
 var userData = {
-  name : '' ,
+  name: '',
   email: '',
-  html:{likes:[], dislikes:[]},
-  js:{likes:[], dislikes:[]},
-  css:{likes:[], dislikes:[]},
-  strength:{css:'',js:'',html:''},
-  currentQuestion:'#welcome'
+  html: {
+    likes: [],
+    dislikes: []
+  },
+  css: {
+    likes: [],
+    dislikes: []
+  },
+  js: {
+    likes: [],
+    dislikes: []
+  },
+  strength: {
+    css: '',
+    js: '',
+    html: ''
+  },
+  currentQuestion: '#welcome'
 };
+
+
 console.log(localStorage.getItem('userData'));
-if(localStorage.getItem(userData)){
-  userData=JSON .parse(localStorage.getItem('userData'));
+
+if(localStorage.getItem('userData')) {
+  userData = JSON.parse(localStorage.getItem('userData'));
   $(userData.currentQuestion).show();
-$('#welcome').hide();
-$('#name').val(userData.name);
-$('#email').val(userData.email);
-}
-else{
-  localStorage.setItem('userData',JSON.stringify(userData));
+  $("#welcome").hide();
+  $("#name").val(userData.name);
+  $("#email").val(userData.email);
+  $("#html").val(userData.html);
+  $("#css").val(userData.css);
+  $("#js").val(userData.js);
+  $("#strength").val(userData.strength);
+} else {
+  localStorage.setItem('userData', JSON.stringify(userData));
 }
 
-$('#start').click(function(){
- 
-  $('#welcome').hide();
-  $('#q1').show();
+$('#start').click(function() {
+  $('#welcome').slideToggle(600);
+  $('#q1').slideToggle(1000);
+  userData.currentQuestion = "#q1";
+  localStorage.setItem('userData', JSON.stringify(userData));
 });
-$('#name').change(function (event) {
-  console.log($('#name').val());
-})
 
-$('#email').change(function (event) {
-  console.log($('#email').val());
-})
+$('#name').change(function(event) {
+  console.log(event);
+});
 
-$('#q1Next').click(function(){
+$('#q1next').click(function() {
+  if($('#name').val() && $('#email').val()) {
+    var nameFlag = false;
+    var emailFlag = false;
+    var namePattern = /^[a-zA-Z ]{1,50}$/;
+    var emailPattern = /^[a-zA-Z0-9]+([\_.-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([\_.-]?[a-zA-Z0-9]+)*(\.[a-zA-Z0-9]{2,4})+$/;
+
+    if($('#name').val().match(namePattern)) {
+      nameFlag = true;
+    }
+    if($('#email').val().match(emailPattern)) {
+      emailFlag = true;
+    }
+    if(nameFlag && emailFlag) {
+      userData.name = $('#name').val();
+      userData.email = $('#email').val();
+      localStorage.setItem('userData', JSON.stringify(userData));
+      $('#q2').slideDown(600);
+      $('#q1').slideUp(600);
+    } else if(!nameFlag) {
+      alert('Enter a valid name');
+    } else if(!emailFlag) {
+      alert('Enter a valid email');
+    }
+  } else {
+    alert('Please enter name and email');
+  }
+});
+
+$('#htmlopen').click(function() {
+  $('#q2a').slideDown(600);
+  $('#q2').slideUp(600);
+  userData.currentQuestion = "#q2a";
+  localStorage.setItem('userData', JSON.stringify(userData));
+});
+
+$('#cssopen').click(function() {
+  $('#q2').hide();
+  $('#q2b').slideToggle(600);
+  userData.currentQuestion = "#q2b";
+  localStorage.setItem('userData', JSON.stringify(userData));
+});
+
+$('#jsopen').click(function() {
+  $('#q2c').slideDown(600);
+  $('#q2').slideToggle(600);
+  userData.currentQuestion = "#q2c";
+  localStorage.setItem('userData', JSON.stringify(userData));
+});
+
+$('#html_prev').click(function() {
+  $('#q2').fadeIn(600);
+  $('#q2a').hide();
+  userData.currentQuestion = "#q2";
+  localStorage.setItem('userData', JSON.stringify(userData));
+});
+
+$('#html_next').click(function() {
+  var likeFlag = false;
+  var dislikeFlag = false;
+  var likeValue = document.getElementsByName('likesHTML');
+  var dislikeValue = document.getElementsByName('dislikesHTML');
   
-  if($('#name').val() &&  $('#email').val()){
-  userData.name = $('#name').val();
-  userData.email = $('#email').val();
-  localStorage.setItem('userData',JSON.stringify(userData));
-  $('#q1').hide();
-  $('#q2').show();
-}
-else{
-  alert('please enter your name and email');
-  $('#q1').hide();
-  $('#q2').show();
+  for(var i = 0; i < likeValue.length; i++) {
+    if (likeValue[i].checked) {
+      userData.html.likes.push(likeValue[i].value);
+      localStorage.setItem('userData', JSON.stringify(userData));
+        likeFlag = true;
+    }
+  }
+  for(var i = 0; i < dislikeValue.length; i++) {
+    if (dislikeValue[i].checked) {
+      userData.html.dislikes.push(dislikeValue[i].value);
+      localStorage.setItem('userData', JSON.stringify(userData));
+        dislikeFlag = true;
+    }
+  }
+  if (likeFlag && dislikeFlag) {
+    if (userData.css.likes.length == 0) {
+        $('#q2a').hide();
+        $('#q2b').slideToggle(400);
+        userData.currentQuestion = "#q2b";
+        localStorage.setItem('userData', JSON.stringify(userData));
+      } else if (userData.js.likes.length == 0) {
+        $('#q2a').fadeOut(600);
+        $('#q2c').fadeIn(600);
+        userData.currentQuestion = "#q2c";
+        localStorage.setItem('userData', JSON.stringify(userData));
+      } else {
+        $('#q2a').fadeOut(600);
+        $('#q3').fadeIn(600);
+        userData.currentQuestion = "#q3";
+        localStorage.setItem('userData', JSON.stringify(userData));
+      }
+  } else {
+    alert('Please select at least 1 checkbox from likes and dislikes!');
+  }
+});
 
-}
+$('#css_prev').click(function() {
+  $('#q2').fadeIn(1000);
+  $('#q2b').hide();
+  userData.currentQuestion = "#q2";
+  localStorage.setItem('userData', JSON.stringify(userData));
+});
+
+$('#css_next').click(function() {
+  var likeFlag = false;
+  var dislikeFlag = false;
+  var likeValue = document.getElementsByName('likesCSS');
+  var dislikeValue = document.getElementsByName('dislikesCSS');
+
+  for (var i = 0; i < likeValue.length; i++) {
+    if(likeValue[i].checked) {
+      userData.css.likes.push(likeValue[i].value);
+      localStorage.setItem('userData', JSON.stringify(userData));
+      likeFlag = true;
+    }
+  }
+  for(var i = 0; i < dislikeValue.length; i++) {
+    if(dislikeValue[i].checked) {
+      userData.css.dislikes.push(dislikeValue[i].value);
+      localStorage.setItem('userData', JSON.stringify(userData));
+      dislikeFlag = true;
+    }
+  }
+
+  if(likeFlag && dislikeFlag) {
+    if(userData.html.likes.length == 0) {
+      $('#q2a').fadeIn(600);
+      $('#q2b').fadeOut(600);
+      userData.currentQuestion = "#q2a";
+      localStorage.setItem('userData', JSON.stringify(userData));
+    } else if(userData.js.likes.length == 0) {
+      $('#q2c').slideDown(600);
+      $('#q2b').slideUp(600);
+      userData.currentQuestion = "#q2c";
+      localStorage.setItem('userData', JSON.stringify(userData));
+    } else {
+      $('#q3').slideToggle(600);
+      $('#q2b').hide();
+      userData.currentQuestion = "#q3";
+      localStorage.setItem('userData', JSON.stringify(userData));
+    }
+  } else {
+    alert("Please select at least 1 checkbox from likes and dislikes!");
+  }
+});
+
+$('#js_prev').click(function() {
+  $('#q2').fadeIn(1000);
+  $('#q2c').hide();
+  userData.currentQuestion = "#q2";
+  localStorage.setItem('userData', JSON.stringify(userData));
+});
+
+$('#js_next').click(function() {
+  var likeFlag = false;
+  var dislikeFlag = false;
+
+  var likeValue = document.getElementsByName("likesJS");
+  var dislikeValue = document.getElementsByName("dislikesJS");
+
+  for(var i = 0; i < likeValue.length; i++) {
+    if(likeValue[i].checked) {
+      userData.js.likes.push(likeValue[i].value);
+      localStorage.setItem('userData', JSON.stringify(userData));
+      likeFlag = true;
+    }
+  }
+  for(var i = 0; i < dislikeValue.length; i++) {
+    if (dislikeValue[i].checked) {
+      userData.js.dislikes.push(dislikeValue[i].value);
+      localStorage.setItem('userData', JSON.stringify(userData));
+      dislikeFlag = true;
+    }
+  }
+  if (likeFlag && dislikeFlag) {
+    if (userData.html.likes.length == 0) {
+      $('#q2a').slideToggle(600);
+      $('#q2c').hide();
+      userData.currentQuestion = "#q2a";
+      localStorage.setItem('userData', JSON.stringify(userData));
+    } else if (userData.css.likes.length == 0) {
+      $('#q2b').fadeIn(600);
+      $('#q2c').fadeOut(600);
+      userData.currentQuestion = "#q2b";
+      localStorage.setItem('userData', JSON.stringify(userData));
+    } else {
+      $('#q3').slideToggle(600);
+      $('#q2c').hide();
+      userData.currentQuestion = "#q3";
+      localStorage.setItem('userData', JSON.stringify(userData));
+    }
+  } else {
+    alert('Please select at least 1 checkbox from likes and dislikes!');
+  }
+});
+
+
+$('#q3_prev').click(function() {
+  $('#q2').slideUp(600);
+  $('#q3').slideDown(600);
+  userData.currentQuestion = "#q2";
+  localStorage.setItem('userData', JSON.stringify(userData));
+});
+
+$('#q3_next').click(function() {
+  var htmlFlag = false;
+  var cssFlag = false;
+  var jsFlag = false;
+
+  var lenHTML = document.getElementsByName('strengthHTML');
+  var lenCSS = document.getElementsByName('strengthCSS');
+  var lenJS = document.getElementsByName('strengthJS');
+
+  for (var i = 0; i < lenHTML.length; i++) {
+      if (lenHTML[i].checked) {
+      userData.strength.html = lenHTML[i].value;
+      localStorage.setItem('userData', JSON.stringify(userData));
+      htmlFlag = true;
+      break;
+      }
+  }
+  for (var i = 0; i < lenCSS.length; i++) {
+    if (lenCSS[i].checked) {
+      userData.strength.css = lenCSS[i].value;
+      localStorage.setItem('userData', JSON.stringify(userData));
+      cssFlag = true;
+      break;
+    }
+  }
+  for (var i = 0; i < lenJS.length; i++) {
+    if (lenJS[i].checked) {
+      userData.strength.js = lenJS[i].value;
+      localStorage.setItem('userData', JSON.stringify(userData));
+      jsFlag = true;
+      break;
+    }
+  }
+  if (htmlFlag && cssFlag && jsFlag) {
+    $('#q3').slideToggle(600);
+    $('#thanks').slideToggle(600);
+    userData.currentQuestion = "#thanks";
+    localStorage.setItem('userData', JSON.stringify(userData));
+  } else {
+    alert("Please select strenths from all fields!");
+  }
+});
+
+// const list = "";
+
+$('#view_answers').click(function() {
+  userData = JSON.parse(localStorage.getItem('userData'));
+  $('#answers').slideToggle(1400);
+  document.getElementsByTagName('h2').innerHTML = "<h2>Your Answers</h2>";
+  // NAME AND EMAIL
+  document.getElementById('showUserName').innerHTML = "<p><b>Your name: </b>" + userData.name + "</p>";
+  document.getElementById('showUserEmail').innerHTML = "<p><b>Your email: </b>" + userData.email + "</p>";
+  // HTML LIKES
+  document.getElementById('htmlLikesHeading').innerHTML = "<p>What do you <b>like</b> about HTML?</p>";
+  for (var i = 0; i < userData.html.likes.length; i++) {
+    var dataList = "<li class='list-group-item'>" + userData.html.likes[i] + "</li>";
+    document.getElementById("htmlLikes").innerHTML += dataList;
+  }
+  // HTML DISLIKES
+  document.getElementById('htmlDislikesHeading').innerHTML = "<p>What do you <b>dislike</b> about HTML?</p>";
+  for (var i = 0; i < userData.html.dislikes.length; i++) {
+    var dataList = "<li class='list-group-item'>" + userData.html.dislikes[i] + "</li>";
+    document.getElementById("htmlDislikes").innerHTML += dataList;
+  }
+  // CSS LIKES
+  document.getElementById('cssLikesHeading').innerHTML = "<p>What do you <b>like</b> about CSS?</p>";
+  for (var i = 0; i < userData.css.likes.length; i++) {
+    var dataList = "<li class='list-group-item'>" + userData.css.likes[i] + "</li>";
+    document.getElementById("cssLikes").innerHTML += dataList;
+  }
+  // CSS DISLIKES
+  document.getElementById('cssDislikesHeading').innerHTML = "<p>What do you <b>dislike</b> about CSS?</p>";
+  for (var i = 0; i < userData.css.dislikes.length; i++) {
+    var dataList = "<li class='list-group-item'>" + userData.css.dislikes[i] + "</li>";
+    document.getElementById("cssDislikes").innerHTML += dataList;
+  }
+  // JS LIKES
+  document.getElementById('jsLikesHeading').innerHTML = "<p>What do you <b>like</b> about JavaScript?</p>";
+  for (var i = 0; i < userData.js.likes.length; i++) {
+    var dataList = "<li class='list-group-item'>" + userData.js.likes[i] + "</li>";
+    document.getElementById("jsLikes").innerHTML += dataList;
+  }
+  // JS DISLIKES
+  document.getElementById('jsDislikesHeading').innerHTML = "<p>What do you <b>dislike</b> about JavaScript?</p>";
+  for (var i = 0; i < userData.js.dislikes.length; i++) {
+    var dataList = "<li class='list-group-item'>" + userData.js.dislikes[i] + "</li>";
+    document.getElementById("jsDislikes").innerHTML += dataList;
+  }
+  // STRENGTH
+  document.getElementById('strengthHeading').innerHTML = "<p><b>Strength</b></p>"
+  document.getElementById('htmlStrength').innerHTML = "<p><b>HTML</b>: " + userData.strength.html + "</p>";
+  document.getElementById('cssStrength').innerHTML = "<p><b>CSS</b>: " + userData.strength.css + "</p>";
+  document.getElementById('jsStrength').innerHTML = "<p><b>JavaScript</b>: " + userData.strength.js + "</p>";
+});
+$('#del_answers').click(function() {
+  $('#welcome').show();
+  $('#thanks').hide();
+  localStorage.removeItem('userData');
+  alert('All your records has deleted!');
+  location.reload(true);
 });
